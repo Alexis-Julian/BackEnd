9fb1ca69d17cfacc8ba87d6cb6ec208fd946177d
Alexis <alexisjrojas@hotmail.es>
2023-07-01 10:40:11 -0300

    Agregadores de errores

--- Entrega6/models/user.model.js
+++ Entrega6/models/user.model.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
+    role: { type: String, required: true },
   },
   { versionKey: false }
 );

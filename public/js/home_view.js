77e151f25964b6c299f377171b51519b66c7f000
Alexis <alexisjrojas@hotmail.es>
2023-07-06 14:39:13 -0300

    Cambios en los fetch y mongoatlas agregado

--- public/js/home_view.js
+++ public/js/home_view.js
@@ -1,9 +1,13 @@
 import getPageQuery from "./utils.js";
 const form = document.getElementById("form_submit");
-function sendProduct(idp) {
+async function sendProduct(idp) {
   let cart = prompt("Intgrese el id del carrito");
-  const url = `http://localhost:8080/api/carts/${cart}/product/${idp}`;
-  fetch(url, { method: "POST" });
+  if (cart) {
+    const url = `http://localhost:8080/api/carts/${cart}/product/${idp}`;
+    let response = await fetch(url, { method: "POST" });
+    let data = await response.json();
+    alert(data.data);
+  }
 }
 form.addEventListener("submit", (e) => {
   e.preventDefault();
--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,22 @@
 const form = document.getElementById("form");
 /* Provisorio  */
+const fetchData = async (login) => {
+  let response = await fetch("http://localhost:8080/api/user/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(login),
+  });
+  let data = await response.json();
+  if (data.status === "SUCCESS") {
+    window.location.href = "http://localhost:8080/view/products";
+  } else {
+    alert(data.data);
+  }
+};
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let [email, password] = e.target;
   let login = { email: email.value, password: password.value };
-  let json = fetch("http://localhost:8080/api/user/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(login),
-  }).then((response) => {
-    return response.json();
-  });
-  json.then((res) => {
-    console.log(res);
-    if (res.status == "SUCCESS") {
-      window.location.href = "http://localhost:8080/view/products";
-    }
-  });
+  fetchData(login);
 });

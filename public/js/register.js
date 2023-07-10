2a14503243db65f00c6c054c2a57d211231d91df
Alexis <alexisjrojas@hotmail.es>
2023-07-10 12:32:57 -0300

    Interfaces graficas mejoradas

--- public/js/register.js
+++ public/js/register.js
@@ -1,23 +1,28 @@
+import { ConfirmPassword } from "/js/utils.js";
 const form = document.getElementById("form");
 /* Provisorio  */
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let [email, username, password] = e.target;
+  let [email, confirmpassword, password] = e.target;
+  let pass = ConfirmPassword(confirmpassword, password);
   let register = {
     email: email.value,
-    username: username.value,
     password: password.value,
   };
-  let json = fetch("http://localhost:8080/api/user/register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(register),
-  }).then((response) => {
-    return response.json();
-  });
-  json.then((res) => {
-    if (res.status == "SUCCESS") {
-      window.location.href = "http://localhost:8080/view/user/product";
-    }
-  });
+  if (pass) {
+    let json = fetch("http://localhost:8080/api/user/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(register),
+    }).then((response) => {
+      return response.json();
+    });
+    json.then((res) => {
+      if (res.status == "SUCCESS") {
+        window.location.href = "http://localhost:8080/view/user/product";
+      }
+    });
+  } else {
+    alert("Nazhe");
+  }
 });
--- public/js/utils.js
+++ public/js/utils.js
@@ -6,3 +6,18 @@ export default function getPageQuery(url, callback) {
   if (!page) page = 1;
   callback(parseInt(page));
 }
+
+export const ConfirmPassword = (password, confpass) => {
+  return password.value == confpass.value;
+};
+
+/* export const Fetch = async (url) => {
+  let url = url;
+  let data = await fetch(url, {
+    method: verb,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(register),
+  });
+  let json = await data.json();
+  return json;
+}; */

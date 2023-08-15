405143239d2e520316bf864cea2f6f4823ba016c
Alexis <alexisjrojas@hotmail.es>
2023-08-15 16:15:16 -0300

    Agregados

--- routes/views/chat_views.routes.js
+++ routes/views/chat_views.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Chat } from "../../controller/views/view_chat.controller.js";
+import { authRequired } from "../../middlewares/validateToken.js";
 export const app = express.Router();
 
 /* Muestra los chats */
-app.get("/", Chat);
+app.get("/", authRequired, Chat);

58fca70d9521184c758df5a8a0da6034a16b8ee9
Alexis <alexisjrojas@hotmail.es>
2023-08-25 11:20:12 -0300

    Agregado de loggers

--- routes/api/logtest.routes.js
+++ routes/api/logtest.routes.js
@@ -3,17 +3,32 @@ import { logger } from "../../utils/logger.js";
 
 export const app = express.Router();
 
-app.get("/error", (req, res) => {});
+app.get("/error", (req, res) => {
+  logger.error("Error received");
+  res.send("Error testing");
+});
 
-app.get("/warn", (req, res) => {});
+app.get("/warn", (req, res) => {
+  logger.warn("Warning received");
+  res.send("Warn testing");
+});
 
-app.get("/info", (req, res) => {});
+app.get("/info", (req, res) => {
+  logger.info("Info received");
+  res.send("Info testing");
+});
 
-app.get("/http", (req, res) => {});
+app.get("/http", (req, res) => {
+  logger.http("http received");
+  res.send("http testing");
+});
 
-app.get("/verbose", (req, res) => {});
+app.get("/verbose", (req, res) => {
+  logger.verbose("Verbose received");
+  res.send("verbose testing");
+});
 
 app.get("/debug", (req, res) => {
-  logger.debug("Un debug normalito");
-  res.send("debug normal");
+  logger.debug("debug received");
+  res.send("debug testing");
 });
--- utils/logger.js
+++ utils/logger.js
@@ -1,16 +1,30 @@
 import winston from "winston";
 import { program } from "../config/enviroment.config.js";
 
-export const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      level: "debug",
-      format: winston.format.combine(winston.format.colorize({ all: true }), winston.format.simple()),
-    }),
-    new winston.transports.File({
-      filename: "./errors.log",
-      level: "warn",
-      format: winston.format.simple(),
-    }),
-  ],
-});
+export const logger = winston.createLogger(program.opts().mode == "DEVELOPMENT" ? ErrorsDevelopment() : ErrorsProduction());
+
+function ErrorsDevelopment() {
+  return {
+    transports: [
+      new winston.transports.Console({
+        level: "debug",
+        format: winston.format.combine(winston.format.colorize({ all: true }), winston.format.simple()),
+      }),
+    ],
+  };
+}
+function ErrorsProduction() {
+  return {
+    transports: [
+      new winston.transports.Console({
+        level: "info",
+        format: winston.format.combine(winston.format.colorize({ all: true }), winston.format.simple()),
+      }),
+      new winston.transports.File({
+        filename: "./errors.log",
+        level: "error",
+        format: winston.format.simple(),
+      }),
+    ],
+  };
+}
